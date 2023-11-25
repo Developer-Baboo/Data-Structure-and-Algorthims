f593fd82d3cd1af648f599d894494440030e2ebe
Developer Baboo <babooheerani@gmail.com>
2023-11-25 12:55:39 +0500

    added other star Patterns

---
+++ Start_Pattern/src/StarPattern2.java
@@ -0,0 +1,10 @@
+public class StarPattern2 {
+    public static void main(String[] args) {
+        for(int i = 1; i<=10; i++){
+            for (int j = 10; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
