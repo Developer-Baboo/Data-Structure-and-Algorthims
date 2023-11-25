dfea73a7a3ec61d254289f70f35946dbe9ef1ee6
Developer Baboo <babooheerani@gmail.com>
2023-11-25 13:24:20 +0500

    added another pattern

---
+++ Start_Pattern/src/StarPattern5.java
@@ -0,0 +1,14 @@
+public class StarPattern5 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++){
+            for (int j = 0; j < 10; j++){
+                if( i==j || i+j==10-1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
